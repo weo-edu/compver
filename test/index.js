3381199c223fea2ff7620390a5abc82e1523f77d
Andrew Shaffer <andrew@weo.io>
2015-11-11 16:04:37 -0800

    work actually

--- test/index.js
+++ test/index.js
@@ -1,6 +1,19 @@
+/**
+ * Imports
+ */
+
 var compver = require('..')
 var assert = require('assert')
 
-describe('compver', function () {
+/**
+ * Tests
+ */
 
+describe('compver', function () {
+  it('should work', function () {
+    assert.equal(compver('1.0.0', '2.0.0'), 'major')
+    assert.equal(compver('1.1.0', '1.2.0'), 'minor')
+    assert.equal(compver('1.1.0', '1.1.1'), 'patch')
+    assert.equal(compver('1.1.1', '1.1.1'), 'same')
+  })
 })
